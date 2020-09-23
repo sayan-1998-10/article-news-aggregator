05dc53149b50fcd268729973c08b9d67befee9f9
Sayan Saha <europa20xx@gmail.com>
2020-09-24 01:18:09 +0530

    ''

--- newsAggregator/newsAggregator/settings.py
+++ newsAggregator/newsAggregator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tbku7x%rr)p2+p4&891vx@55y40qa3qgr!3cxzf+91hdomv-$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
