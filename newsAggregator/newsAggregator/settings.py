1face1cb3c417808ab3ec9ab15d76c51736950a4
Sayan Saha <europa20xx@gmail.com>
2020-09-24 01:00:25 +0530

    'Second_Commit'

--- newsAggregator/newsAggregator/settings.py
+++ newsAggregator/newsAggregator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tbku7x%rr)p2+p4&891vx@55y40qa3qgr!3cxzf+91hdomv-$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['article-news-aggregator.herokuapp.com','127.0.0.1']
 
 
 # Application definition
